5393e891c00f9232212ee6500d9a45e0581f8e4f
Luka Dobrijevic <lukadob@Lukas-MacBook-Pro.local>
2020-08-20 20:23:42 -0600

    Created an array of 10 strings and made a loop that displays them all to console

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var tweets = ["This", "array", "definitely", "has", "ten", "words", "in", "it", "as", "strings"];
+var arrayLength = tweets.length;
+
+
+for(var index = 0; index < arrayLength; index++) {
+    console.log (tweets[index]);
+}
